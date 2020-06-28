98057d59f116f629079c2aa978c7db460e0995af
김현준 <k2i2m2@gimhyeonjun-ui-MacBook-Pro.local>
2020-06-28 16:03:14 +0900

    update node

---
+++ server.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+
+// 상수
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// 앱
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, HOST);
